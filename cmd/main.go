312e0007fc9ea7e88cbc04dab667639b031d19e4
JoshuaHenriques <joshuahenriques95@gmail.com>
2024-05-15 19:47:11 -0400

    configure and implement db storage

--- cmd/main.go
+++ cmd/main.go
@@ -1,15 +1,35 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"log"
 
-	_ "github.com/joho/godotenv/autoload"
 	"github.com/joshuahenriques/go-ecom/cmd/api"
+	"github.com/joshuahenriques/go-ecom/config"
+	"github.com/joshuahenriques/go-ecom/db"
 )
 
 func main() {
-	server := api.NewAPIServer(":3000", nil)
+	connStr := fmt.Sprintf("postgresql://%s:%s@%s/%s?sslmode=disable", config.Envs.DBUser, config.Envs.DBPassword, config.Envs.DBAddress, config.Envs.DBName)
+	db, err := db.NewPostgresStorage(connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	initStorage(db)
+
+	server := api.NewAPIServer(fmt.Sprintf(":%s", config.Envs.Port), db)
 	if err := server.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func initStorage(db *sql.DB) {
+	err := db.Ping()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	log.Println("DB: Successfully connected!")
 }
---
+++ config/env.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"fmt"
+	"os"
+
+	_ "github.com/joho/godotenv/autoload"
+)
+
+type Config struct {
+	PublicHost string
+	Port       string
+	DBUser     string
+	DBPassword string
+	DBAddress  string
+	DBName     string
+}
+
+var Envs = initConfig()
+
+func initConfig() Config {
+	return Config{
+		PublicHost: getEnv("PUBLIC_HOST", "http://localhost"),
+		Port:       getEnv("PORT", "8080"),
+		DBUser:     getEnv("DB_USER", "root"),
+		DBPassword: getEnv("DB_PASSWORD", "password"),
+		DBAddress:  fmt.Sprintf("%s:%s", getEnv("DB_HOST", "localhost"), getEnv("DB_PORT", "5432")),
+		DBName:     getEnv("DB_NAME", "database"),
+	}
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
